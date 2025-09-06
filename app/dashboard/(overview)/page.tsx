dc8584094fe0b2f51dbd5c9f6ddef1342b3b5c1a
Vivek <vivek.mitra@outlook.com>
2025-09-06 13:00:35 +1000

    Deliberately introduced lag to learn React Suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,12 @@
 import { Card } from "../../ui/dashboard/cards";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../../lib/data";
+import { fetchLatestInvoices, fetchCardData } from "../../lib/data";
 import { lusitana } from "../../ui/fonts";
+import { Suspense } from "react";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfCustomers,
         numberOfInvoices,
@@ -29,8 +29,13 @@ export default async function Page() {
                 />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue}  />
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+                
             </div>
         </main>
     );
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,6 +32,10 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    //Artifically manipulating late fetch
+    console.log("Fetching Latest Invoices");
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -43,6 +47,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    console.log("Fetch completed after 5 seconds");
     return latestInvoices;
   } catch (error) {
     console.error("Database Error:", error);
