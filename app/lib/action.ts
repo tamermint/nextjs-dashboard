5abc710fb7facd6107c430afd3c7181370fc64f6
Vivek <vivek.mitra@outlook.com>
2025-09-12 18:28:13 +1000

    edit and delete invoice functionality added

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -9,17 +9,18 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
-  customer_id: z.string(),
+  customerid: z.string(),
   amount: z.coerce.number(),
   status: z.enum(["pending", "paid"]),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const { customer_id, amount, status } = CreateInvoice.parse({
-    customer_id: formData.get("customerId"),
+  const { customerid, amount, status } = CreateInvoice.parse({
+    customerid: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
@@ -28,9 +29,32 @@ export async function createInvoice(formData: FormData) {
 
   await sql`
   INSERT INTO invoices(customer_id, amount, status, date)
-  VALUES(${customer_id}, ${amount}, ${status}, ${date})
+  VALUES(${customerid}, ${amount}, ${status}, ${date})
   `;
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerid, amount, status } = UpdateInvoice.parse({
+    customerid: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+
+  await sql`
+  UPDATE invoices
+  SET customer_id = ${customerid}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices where id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
